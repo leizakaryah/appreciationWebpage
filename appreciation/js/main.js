c70a50ee48efb9e43de3346c6954d65130746395
leizakaryah <72323308+leizakaryah@users.noreply.github.com>
2021-07-29 10:42:28 -0700

    Add files via upload

---
+++ appreciation/js/main.js
@@ -0,0 +1,21 @@
+$(".desc").hide()
+
+function skidaddle(name) {  
+    $(name).slideToggle()
+};
+
+$("#abbyBut").on("click", function() {skidaddle("#abbyDesc")})
+$("#abeBut").on("click", function() {skidaddle("#abeDesc")})
+$("#ansBut").on("click", function() {skidaddle("#ansDesc")})
+$("#antBut").on("click", function() {skidaddle("#antDesc")})
+$("#benBut").on("click", function() {skidaddle("#benDesc")})
+$("#favBut").on("click", function() {skidaddle("#favDesc")})
+$("#gabbyBut").on("click", function() {skidaddle("#gabbyDesc")})
+$("#hanBut").on("click", function() {skidaddle("#hanDesc")})
+$("#jhoBut").on("click", function() {skidaddle("#jhoDesc")})
+$("#kevBut").on("click", function() {skidaddle("#kevDesc")})
+$("#krisBut").on("click", function() {skidaddle("#krisDesc")})
+$("#leiBut").on("click", function() {skidaddle("#leiDesc")})
+$("#luisBut").on("click", function() {skidaddle("#luisDesc")})
+$("#pabBut").on("click", function() {skidaddle("#pabDesc")})
+$("#quiBut").on("click", function() {skidaddle("#quiDesc")})
